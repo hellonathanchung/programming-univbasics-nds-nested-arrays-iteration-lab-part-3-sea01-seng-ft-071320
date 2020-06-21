da7335556bc7974e87616324370c61f63eea6193
Nathan Chung <chung.nathanh@gmail.com>
2020-06-21 04:58:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row_index < src.count do
   element_index = 0
   while element_index< src[row_index].count do
     if += src[row_index][element_index]
-    elementindex += 1 
+    final_string += src[row_index[element_index]
   end 
   row_index += 1 
 end
