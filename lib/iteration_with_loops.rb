f38735acc99dc0a33cbd205510b8e06f6ce545d9
Nathan Chung <chung.nathanh@gmail.com>
2020-06-21 04:56:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,4 +3,9 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   
 sentence = ""
+row index = 0 
+while row_index < src.count do 
+  element_index = 0
+  while element_index< src[row_index].count do
+    total += src[row_index][element_index]
 end
